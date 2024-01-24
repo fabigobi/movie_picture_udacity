5e82d5fe50240dd17cee89640a98e11fac8a781e
fabigobi <gobifabi@gmail.com>
2024-01-24 16:07:58 +0000

    fix

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
       // Customize your rules here
       'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
       'react/jsx-uses-vars': 'warn',
-      'endOfLine': 'auto'
+      'endOfLine': '1'
     },
   };
   
